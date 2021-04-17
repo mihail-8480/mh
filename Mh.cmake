7ee3c45ebdd773a11507ff56f6c9b66c068c6eef
mihail <mihail-mojsoski@outlook.com>
2021-04-17 21:49:52 +0200

    Fixed problem

--- Mh.cmake
+++ Mh.cmake
@@ -1,4 +1,6 @@
 cmake_minimum_required(VERSION 3.0)
+
+# OS-specific stuff
 if(UNIX AND NOT (APPLE OR HAIKU))
     add_compile_definitions(UNIX_LIKE)
     set(UNIX_LIKE 1)
@@ -22,10 +24,17 @@ if (HAIKU)
     add_compile_definitions(_BSD_SOURCE)
 endif(HAIKU)
 
+# If you build using "Debug" enable MH_DEBUG
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
     add_compile_definitions(MH_DEBUG)
 endif (CMAKE_BUILD_TYPE STREQUAL "Debug")
 
+# Set the C compiler flags
 set(CMAKE_C_STANDARD 99)
 set(CMAKE_C_FLAGS_DEBUG "-ggdb3")
 set(CMAKE_C_FLAGS_RELEASE "-O3")
+
+# Set the output to "bin"
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
