7fcb0e53eba28b84fb36d63e2b3de84b085f378c
mihail <mihail-mojsoski@outlook.com>
2021-05-02 18:35:33 +0200

    ~ <mh_tests.c> mh_tests_run(): Improved the language.

--- src/mh_tests.c
+++ src/mh_tests.c
@@ -9,20 +9,20 @@ MH_NORETURN void mh_tests_run(const mh_test_t* tests, size_t count) {
             failed++;
             continue;
         }
-        printf("[......] [%zu/%zu] Running test `%s`...", i+1, count, tests[i].name);
+        printf("[......] [%zu/%zu] Running the test `%s`...", i+1, count, tests[i].name);
         fflush(stdout);
         mh_test_return_t result = tests[i].func();
         if (!result.success) {
             char loc[128];
             mh_code_location_to_string(loc, result.location);
-            printf("\r[FAILED] [%zu/%zu] Failed test `%s` because \"%s\" %s.\n", i+1, count, tests[i].name, result.reason, loc);
+            printf("\r[FAILED] [%zu/%zu] The test `%s` has failed because \"%s\" %s.\n", i+1, count, tests[i].name, result.reason, loc);
             failed++;
             if (tests[i].required) {
-                fprintf(stderr,"%s is a required test, stopping...\n", tests[i].name);
+                fprintf(stderr,"`%s` is a required test, stopping...\n", tests[i].name);
                 exit(1);
             }
         } else {
-            printf("\r[PASSED] [%zu/%zu] Passed test `%s`.\n", i+1, count, tests[i].name);
+            printf("\r[PASSED] [%zu/%zu] The test `%s` has passed.\n", i+1, count, tests[i].name);
         }
     }
     printf("Finished testing and ");
