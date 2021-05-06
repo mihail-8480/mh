346ce33c1e5238c846d68c3259be080296bcf12d
mihail-8480 <mihail-mojsoski@outlook.com>
2021-05-06 02:32:13 -0700

    Made the handle test work on windows

--- tests/handle_test.c
+++ tests/handle_test.c
@@ -3,8 +3,14 @@
 
 typedef mh_version_t (*mh_get_version_t)(void);
 
+#ifdef WIN32
+static const char* libmh_lib = "libmh.dll";
+#else
+static const char* libmh_lib = "libmh.so";
+#endif
+
 MH_TEST_CONTEXT_NEW(handle_test, {
-    mh_handle_t *handle = mh_handle_new(context, "libmh.so");
+    mh_handle_t *handle = mh_handle_new(context, libmh_lib);
     mh_get_version_t libmh_get_version = (mh_get_version_t) (size_t) mh_handle_find_symbol(handle, "mh_get_version");
     mh_version_t version = libmh_get_version();
     MH_TEST_CONTEXT_EXPECT(version.patch + version.minor + version.major > 0);
