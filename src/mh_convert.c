15e4b8abf2a31fd7fb99a4bde372bb96fb90c386
mihail <mihail-mojsoski@outlook.com>
2021-05-12 01:01:07 +0200

    Fixed build on haiku and optimized stuff in <mh_convert.h>'s implementation.

--- src/mh_convert.c
+++ src/mh_convert.c
@@ -1,16 +1,15 @@
 #include "../inc/mh_convert.h"
+#include <math.h>
 
-static inline size_t mh_intlen(mh_unsigned_number_t num) {
-    size_t i;
-    for (i = 0; num; num/=10, i++);
-    return i;
+static inline size_t mh_intlen10(mh_unsigned_number_t num) {
+    return (size_t)ceil(log10(num));
 }
 
 static inline mh_unsigned_number_t mh_abs(mh_signed_number_t num) {
     return (num >= 0) ? num : -num;
 }
 
-static inline bool mh_strint(mh_memory_t *memory, mh_unsigned_number_t num, size_t end) {
+static inline bool mh_strint10(mh_memory_t *memory, mh_unsigned_number_t num, size_t end) {
     if (end >= memory->size) {
         return false;
     }
@@ -25,8 +24,8 @@ static inline bool mh_strint(mh_memory_t *memory, mh_unsigned_number_t num, size
 
 bool mh_int_to_string(mh_memory_t *memory, mh_signed_number_t num) {
     mh_unsigned_number_t p_num = mh_abs(num);
-    size_t end = mh_intlen(p_num) + (num < 0);
-    bool res = mh_strint(memory, p_num, end);
+    size_t end = mh_intlen10(p_num) + (num < 0);
+    bool res = mh_strint10(memory, p_num, end);
     if (num < 0) {
         ((char*)memory->address)[0] = '-';
     }
@@ -34,5 +33,5 @@ bool mh_int_to_string(mh_memory_t *memory, mh_signed_number_t num) {
 }
 
 bool mh_uint_to_string(mh_memory_t *memory, mh_unsigned_number_t num) {
-    return mh_strint(memory, num, mh_intlen(num));
+    return mh_strint10(memory, num, mh_intlen10(num));
 }
