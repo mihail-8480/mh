dbf6d7692d9d404fdd1eb4e77e37fbe7179cfa6e
mihail <mihail-mojsoski@outlook.com>
2021-05-12 01:12:59 +0200

    Removed some unused functions from the hashmap.

--- ext/hashmap/hashmap.c
+++ ext/hashmap/hashmap.c
@@ -178,8 +178,6 @@ void *hashmap_set(struct hashmap *map, void *item) {
     }
 }
 
-// hashmap_get returns the item based on the provided key. If the item is not
-// found then NULL is returned.
 void *hashmap_get(struct hashmap *map, void *key) {
     if (!key) {
         panic("Key is null.");
@@ -248,18 +246,10 @@ void *hashmap_delete(struct hashmap *map, void *key) {
     }
 }
 
-size_t hashmap_count(struct hashmap *map) {
-    return map->count;
-}
-
 void hashmap_free(struct hashmap *map) {
     if (!map) return;
     map->free(map->buckets);
     map->free(map);
-}
-
-bool hashmap_oom(struct hashmap *map) {
-    return map->oom;
 }
 
 bool hashmap_scan(struct hashmap *map,
@@ -376,162 +366,7 @@ static uint64_t SIP64(const uint8_t *in, const size_t inlen,
     return out;
 }
 
-//-----------------------------------------------------------------------------
-// MurmurHash3 was written by Austin Appleby, and is placed in the public
-// domain. The author hereby disclaims copyright to this source code.
-//
-// Murmur3_86_128
-//-----------------------------------------------------------------------------
-#define    ROTL32(x, r) (((x) << (r)) | ((x) >> (32 - (r))))
-#define FMIX32(h) h^=(h)>>16; (h)*=0x85ebca6b; (h)^=(h)>>13; (h)*=0xc2b2ae35; (h)^=(h)>>16;
-
-static void MM86128(const void *key, const int len, uint32_t seed, void *out) {
-    const uint8_t *data = (const uint8_t *) key;
-    const int nblocks = len / 16;
-    uint32_t h1 = seed;
-    uint32_t h2 = seed;
-    uint32_t h3 = seed;
-    uint32_t h4 = seed;
-    uint32_t c1 = 0x239b961b;
-    uint32_t c2 = 0xab0e9789;
-    uint32_t c3 = 0x38b34ae5;
-    uint32_t c4 = 0xa1e38b93;
-    const uint32_t *blocks = (const uint32_t *) (data + nblocks * 16);
-    for (int i = -nblocks; i; i++) {
-        uint32_t k1 = blocks[i * 4 + 0];
-        uint32_t k2 = blocks[i * 4 + 1];
-        uint32_t k3 = blocks[i * 4 + 2];
-        uint32_t k4 = blocks[i * 4 + 3];
-        k1 *= c1;
-        k1 = ROTL32(k1, 15);
-        k1 *= c2;
-        h1 ^= k1;
-        h1 = ROTL32(h1, 19);
-        h1 += h2;
-        h1 = h1 * 5 + 0x561ccd1b;
-        k2 *= c2;
-        k2 = ROTL32(k2, 16);
-        k2 *= c3;
-        h2 ^= k2;
-        h2 = ROTL32(h2, 17);
-        h2 += h3;
-        h2 = h2 * 5 + 0x0bcaa747;
-        k3 *= c3;
-        k3 = ROTL32(k3, 17);
-        k3 *= c4;
-        h3 ^= k3;
-        h3 = ROTL32(h3, 15);
-        h3 += h4;
-        h3 = h3 * 5 + 0x96cd1c35;
-        k4 *= c4;
-        k4 = ROTL32(k4, 18);
-        k4 *= c1;
-        h4 ^= k4;
-        h4 = ROTL32(h4, 13);
-        h4 += h1;
-        h4 = h4 * 5 + 0x32ac3b17;
-    }
-    const uint8_t *tail = (const uint8_t *) (data + nblocks * 16);
-    uint32_t k1 = 0;
-    uint32_t k2 = 0;
-    uint32_t k3 = 0;
-    uint32_t k4 = 0;
-    switch (len & 15) {
-        case 15:
-            k4 ^= tail[14] << 16;
-            MH_FALLTHROUGH
-        case 14:
-            k4 ^= tail[13] << 8;
-            MH_FALLTHROUGH
-        case 13:
-            k4 ^= tail[12] << 0;
-            k4 *= c4;
-            k4 = ROTL32(k4, 18);
-            k4 *= c1;
-            h4 ^= k4;
-            MH_FALLTHROUGH
-        case 12:
-            k3 ^= tail[11] << 24;
-            MH_FALLTHROUGH
-        case 11:
-            k3 ^= tail[10] << 16;
-            MH_FALLTHROUGH
-        case 10:
-            k3 ^= tail[9] << 8;
-            MH_FALLTHROUGH
-        case 9:
-            k3 ^= tail[8] << 0;
-            k3 *= c3;
-            k3 = ROTL32(k3, 17);
-            k3 *= c4;
-            h3 ^= k3;
-            MH_FALLTHROUGH
-        case 8:
-            k2 ^= tail[7] << 24;
-            MH_FALLTHROUGH
-        case 7:
-            k2 ^= tail[6] << 16;
-            MH_FALLTHROUGH
-        case 6:
-            k2 ^= tail[5] << 8;
-            MH_FALLTHROUGH
-        case 5:
-            k2 ^= tail[4] << 0;
-            k2 *= c2;
-            k2 = ROTL32(k2, 16);
-            k2 *= c3;
-            h2 ^= k2;
-            MH_FALLTHROUGH
-        case 4:
-            k1 ^= tail[3] << 24;
-            MH_FALLTHROUGH
-        case 3:
-            k1 ^= tail[2] << 16;
-            MH_FALLTHROUGH
-        case 2:
-            k1 ^= tail[1] << 8;
-            MH_FALLTHROUGH
-        case 1:
-            k1 ^= tail[0] << 0;
-            k1 *= c1;
-            k1 = ROTL32(k1, 15);
-            k1 *= c2;
-            h1 ^= k1;
-    }
-    h1 ^= len;
-    h2 ^= len;
-    h3 ^= len;
-    h4 ^= len;
-    h1 += h2;
-    h1 += h3;
-    h1 += h4;
-    h2 += h1;
-    h3 += h1;
-    h4 += h1;
-    FMIX32(h1)
-    FMIX32(h2)
-    FMIX32(h3)
-    FMIX32(h4)
-    h1 += h2;
-    h1 += h3;
-    h1 += h4;
-    h2 += h1;
-    h3 += h1;
-    h4 += h1;
-    ((uint32_t *) out)[0] = h1;
-    ((uint32_t *) out)[1] = h2;
-    ((uint32_t *) out)[2] = h3;
-    ((uint32_t *) out)[3] = h4;
-}
-
 uint64_t hashmap_sip(const void *data, size_t len,
                      uint64_t seed0, uint64_t seed1) {
     return SIP64((uint8_t *) data, len, seed0, seed1);
-}
-
-uint64_t hashmap_murmur(const void *data, size_t len,
-                        uint64_t seed0, __attribute__((unused)) uint64_t seed1) {
-    char out[16];
-    MM86128(data, (int) len, seed0, &out);
-    return *(uint64_t *) out;
 }
