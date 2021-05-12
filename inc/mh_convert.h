a806cdad544c6c74059a35e8c35266c737a55bc0
mihail <mihail-mojsoski@outlook.com>
2021-05-12 18:29:49 +0200

    Added number conversion support for more number systems.

--- inc/mh_convert.h
+++ inc/mh_convert.h
@@ -3,6 +3,14 @@
 
 #include "mh_memory.h"
 
+// Represents a number system.
+typedef enum mh_base {
+    MH_BASE_BIN = 2,
+    MH_BASE_OCT = 8,
+    MH_BASE_DEC = 10,
+    MH_BASE_HEX = 16
+} mh_base_t;
+
 // A signed number.
 MH_API_TYPE(mh_signed_number, int64_t);
 
@@ -10,9 +18,9 @@ MH_API_TYPE(mh_signed_number, int64_t);
 MH_API_TYPE(mh_unsigned_number, u_int64_t);
 
 // Convert a signed number to a string and move the offset to the amount of characters written.
-MH_API_FUNC(bool mh_int_to_string(mh_memory_t *memory, mh_signed_number_t num));
+MH_API_FUNC(bool mh_int_to_string(mh_memory_t *memory, mh_signed_number_t num, mh_base_t base));
 
 // Convert an unsigned number to a string and move the offset to the amount of characters written.
-MH_API_FUNC(bool mh_uint_to_string(mh_memory_t *memory, mh_unsigned_number_t num));
+MH_API_FUNC(bool mh_uint_to_string(mh_memory_t *memory, mh_unsigned_number_t num, mh_base_t base));
 
 #endif //MH_MH_CONVERT_H
