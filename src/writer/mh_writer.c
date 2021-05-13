81f2038f194362a2212728d8b2b031526f4bc82d
mihail <mihail-mojsoski@outlook.com>
2021-05-13 16:57:52 +0200

    <mh_writer.h> + mh_writer_t: A type that can be written to. + mh_write_string(): Write a string to a writer. + mh_write_memory(): Write memory into a writer. + mh_write_signed_number(): Write a signed number into a writer. + mh_write_unsigned_number(): Write an unsigned number into a writer.
    <mh_stream.h>
    + mh_writer_from_stream(): Get a mh_writer_t from a stream.

---
+++ src/writer/mh_writer.c
@@ -0,0 +1,24 @@
+#include "mh_writer_private.h"
+
+void mh_write_string(const mh_writer_t *writer, const char* str) {
+    mh_memory_t memory = MH_STRING((void*)str);
+    writer->write(writer->instance, &memory, memory.size);
+}
+
+void mh_write_memory(const mh_writer_t *writer, mh_memory_t memory) {
+    writer->write(writer->instance, &memory, memory.size);
+}
+
+void mh_write_signed_number(const mh_writer_t *writer, mh_signed_number_t num) {
+    char data[20];
+    mh_memory_t mem = MH_REF_CONST(data);
+    mh_int_to_string(&mem, num, 10);
+    writer->write(writer->instance, &mem, mem.offset);
+}
+
+void mh_write_unsigned_number(const mh_writer_t *writer, mh_unsigned_number_t num) {
+    char data[20];
+    mh_memory_t mem = MH_REF_CONST(data);
+    mh_uint_to_string(&mem, num, 10);
+    writer->write(writer->instance, &mem, mem.offset);
+}
