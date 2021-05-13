4d1814075d19e3a2703e60efc931dbfd67da101a
mihail <mihail-mojsoski@outlook.com>
2021-05-13 17:13:58 +0200

    <mh_console.h> - MH_OUT, MH_ERR, MH_PRINT*(): This macros were removed in favor of the mh_writer_t functions. + mh_console_error, mh_console_output: mh_writer_t wrappers on top of the streams.

--- inc/mh_console.h
+++ inc/mh_console.h
@@ -14,6 +14,9 @@
 extern mh_stream_t *mh_console_output_stream;
 extern mh_stream_t *mh_console_error_stream;
 
+extern mh_writer_t *mh_console_error;
+extern mh_writer_t *mh_console_output;
+
 // The argument parser settings.
 MH_API_TYPE(mh_argument_parser_args, struct mh_argument_parser_args {
     // Required arguments with a space in between.
@@ -27,14 +30,7 @@ MH_API_FUNC(mh_map_t *
             mh_argument_parse(
                     mh_context_t *context, const mh_argument_parser_args_t *args, int argc, char *argv[]));
 
-#define MH_OUT mh_console_output_stream
-#define MH_ERR mh_console_error_stream
-
-#define MH_PRINT(c_str) mh_stream_write_reference(MH_OUT, c_str, sizeof(c_str)-1)
-#define MH_PRINT_STR(str) mh_stream_write_reference(MH_OUT, str, strlen(str))
-#define MH_PRINT_MEM(mem) mh_stream_write(MH_OUT, &mem, mem.size)
-#define MH_PRINT_ERROR(c_str) mh_stream_write_reference(MH_ERR, c_str, sizeof(c_str)-1)
-#define MH_PRINT_ERROR_STR(str) mh_stream_write_reference(MH_ERR, str, strlen(str))
-#define MH_PRINT_ERROR_MEM(mem) mh_stream_write(MH_ERR, &mem, mem.size)
+#define MH_OUTPUT mh_console_output
+#define MH_ERROR mh_console_error
 
 #endif //MH_MH_CONSOLE_H
