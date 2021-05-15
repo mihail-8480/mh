0fd9c621c2495b824a1c13a25eef3756188b3127
mihail-8480 <mihail-mojsoski@outlook.com>
2021-05-15 10:01:12 -0700

    Fixed compiling on windows, the TCP test fails.

--- inc/mh_convert.h
+++ inc/mh_convert.h
@@ -2,6 +2,7 @@
 #define MH_MH_CONVERT_H
 
 #include "mh_memory.h"
+#include <stdint.h>
 
 /*
  * <mh_convert.h>
@@ -17,10 +18,10 @@ typedef enum mh_base {
 } mh_base_t;
 
 // A signed number.
-MH_API_TYPE(mh_signed_number, int64_t);
+MH_API_TYPE(mh_signed_number, intmax_t);
 
 // An unsigned number.
-MH_API_TYPE(mh_unsigned_number, u_int64_t);
+MH_API_TYPE(mh_unsigned_number, uintmax_t);
 
 // Convert a signed number to a string and move the offset to the amount of characters written.
 MH_API_FUNC(bool mh_int_to_string(mh_memory_t *memory, mh_signed_number_t num, mh_base_t base));
